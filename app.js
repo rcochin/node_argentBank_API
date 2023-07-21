58f9507e1437e4a00767e6417efe00adae8daf1d
rcochin <romain.cochin.01@gmail.com>
2023-07-21 11:37:25 +0200

    Add post and get User + connect DB

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
-app.use((req, res, next) =>{
-    console.log("requête reçu !");
-    next();
-});
+mongoose.connect('mongodb://127.0.0.1:27017/argentBankDB',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-app.use((req, res, next) =>{
-    res.status(201);
-    next();
-});
+app.use(express.json());
 
 app.use((req, res, next) => {
-    res.json({ message: 'Votre requête a bien été reçu'});
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
-});
-
-app.use((req, res) =>{
-    console.log("La réponse a été envoyée avec succès !")
 })
 
+app.get('/api/users', (req, res, next) => {
 
+})
+
+app.post('/api/users', (req, res, next) => {
+
+})
 module.exports = app;
